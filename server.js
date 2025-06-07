51d5cd0cd32ae7a706275f31ece5a88f2a45c6a7
queuegeforcenow <queuegeforcenow@gmail.com>
2025-06-07 20:00:42 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-const authController = require('./backend/controllers/authController');
+const { register, login } = require('./backend/controllers/authController');
 
 const app = express();
 
